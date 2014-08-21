0ef466670b784797b82c7af54ba5bf9551da7807
Tully Gray <shadowdaemon@anapnea.net>
2014-08-21 22:57:13 +1000

    Forgot this.

--- render.c
+++ render.c
@@ -464,7 +464,7 @@ void render(GLFWwindow *window, struct aiScene *scene, struct aiScene *textquads
   color[0] += lpos[0] > 800 ? (lpos[0] - 800) * 0.005f * 0.1f : 0;
   color[1] = 0.5176470588235295f * (lpos[1] > -400 ? (lpos[1] + 400) / 1400.0f : 0);
   color[2] = 0.8431372549019608f * (lpos[1] > -200 ? 0.05f + (lpos[1] + 200) / 1200.0f : 0.05f);
-  updateFog(color, *squaresize, fogend);
+  updateFog(color, *squaresize, fogend, camerapos.y);
   glClearColor(color[0], color[1], color[2], color[3]);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
   glShadeModel(GL_SMOOTH);
